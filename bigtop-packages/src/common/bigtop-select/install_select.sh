bf3bd5b52b4b491a0b36d00b684807f6082d2d84
Nazarii Melnyk <nmelnyk@rework-space.com>
2024-08-09 12:55:50 +0300

    Enable bigtop-select tool on v3.2.1
    Signed-off-by: Nazarii Melnyk <nmelnyk@rework-space.com>

--- bigtop-packages/src/common/bigtop-select/install_select.sh
+++ bigtop-packages/src/common/bigtop-select/install_select.sh
@@ -27,7 +27,6 @@ usage: $0 <options>
      --parent-dir=DIR                             path to install stack-root/stack-version
      --bin-dir=DIR                                path to bin directory
      --lib-dir=DIR                                path to lib directory
-     --conf-dir=DIR                               path to conf directory
      --bigtop-base-version=BIGTOP_BASE_VERSION    Bigtop Base Version
   "
   exit 1
@@ -68,9 +67,6 @@ while true ; do
         --lib-dir)
         LIB_DIR=$2 ; shift 2
         ;;
-        --conf-dir)
-        CONF_DIR=$2 ; shift 2
-        ;;
         --bigtop-base-version)
         BIGTOP_BASE_VERSION=$2 ; shift 2
         ;;
@@ -85,44 +81,18 @@ while true ; do
     esac
 done
 
-DIST_DIR=${BUILD_DIR}/dist
-MAN_DIR=${MAN_DIR:-/usr/share/man/man1}
-DOC_DIR=${DOC_DIR:-/usr/share/doc/bigtop-select}
-BIN_DIR=${BIN_DIR:-/usr/bin}
+BIGTOP_BASE_VERSION=${BIGTOP_BASE_VERSION:-"1.0"}
 LIB_DIR=${LIB_DIR:-/usr/lib/bigtop-select}
-CONF_DIR=${CONF_DIR:-/etc/bigtop-select/conf.dist}
+BIN_DIR=${BIN_DIR:-${LIB_DIR}/${BIGTOP_BASE_VERSION}/bin}
+DISTRO_DIR=${DISTRO_DIR:-${BUILD_DIR}/debian}
 
-STACK_ROOT_DIR=$(echo "${PARENT_DIR}" | sed -e "s/\/${BIGTOP_BASE_VERSION}$//")
 STACK_SELECTOR=distro-select
 CONF_SELECTOR=conf-select
 
 # Install packages
-install -d -p -m 755 "$PREFIX""${LIB_DIR}"/
-install -d -p -m 755 "$PREFIX""${PARENT_DIR}"/
-install -p -m 755 "${DISTRO_DIR}"/${STACK_SELECTOR} "$PREFIX""${LIB_DIR}"/
-install -p -m 755 "${DISTRO_DIR}"/${CONF_SELECTOR} "$PREFIX""${LIB_DIR}"/
-cat > "$PREFIX""${LIB_DIR}"/params.py <<EOF
-#!/usr/bin/python2
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+install -d -p -m 755 "${PREFIX}""${PARENT_DIR}"/
+install -d -p -m 755 "${PREFIX}"/"${BIN_DIR}"/
+install -d -p -m 755 "${PREFIX}""${LIB_DIR}"/current/
 
-stack_root = '${STACK_ROOT_DIR}'
-stack_version = '${BIGTOP_BASE_VERSION}'
-
-EOF
-chmod 755 "$PREFIX""${LIB_DIR}"/params.py
+install -p -m 755 "${DISTRO_DIR}"/${STACK_SELECTOR} "${PREFIX}"/"${BIN_DIR}"/
+install -p -m 755 "${DISTRO_DIR}"/${CONF_SELECTOR} "${PREFIX}"/"${BIN_DIR}"/
